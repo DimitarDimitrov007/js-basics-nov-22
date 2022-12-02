d7f9f365c7f8065a217f6c456b43e7cccd399c45
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2022-12-02 15:55:51 +0200

    Solutions for For Loop - Lab
    Solutions with 100/100 SoftUni Judge mark.

---
+++ For Loop - Lab/characterSequence.js
@@ -0,0 +1,11 @@
+function characterSequence(input){
+    let text = input[0];
+
+    for(let i = 0; i < text.length; i+=1){
+        console.log(text[i]);
+
+    }
+
+}
+characterSequence(["softuni"]);
+characterSequence(["ice cream"]);
---
+++ For Loop - Lab/numbersN...1.js
@@ -0,0 +1,14 @@
+function numbersNto1(input){
+    let n = Number(input[0]);
+
+    for(let a = n; a >= 1; a-=1 ){
+        console.log(a);
+
+    }
+
+
+
+}
+numbersNto1(["2"]);
+numbersNto1(["3"]);
+numbersNto1(["5"]);
