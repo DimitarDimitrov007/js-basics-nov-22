8fcf4e9dc8c3f5a49a65fa1d078f95cfa78b36d0
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2022-12-09 19:32:45 +0200

    Add files via upload

---
+++ For Loop More Exercises/Equal Pairs.js
@@ -0,0 +1,12 @@
+function equalPairs(input){
+    let numbersCount =  2* Number(input[0]);
+
+    for(let i = 1; i <= numbersCount; i+=1){
+        
+
+        
+    }
+
+
+}
+equalPairs([3,1,2,0,3,4,-1]);
